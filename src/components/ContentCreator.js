74d03bc9820069b50c4611f1916e1059fe55f300
GalZilca <galzilca@gmail.com>
2020-07-25 18:30:46 +0300

    create ContentCreator

--- src/components/ContentCreator.js
+++ src/components/ContentCreator.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from 'react-router-dom';
 
+import { Editor } from '@tinymce/tinymce-react';
+
 import { Header } from "./Header"
 
 export const ContentCreator = () => (
@@ -16,10 +18,28 @@ export const ContentCreator = () => (
                 <input className="input__content" type="test" name="mail" placeholder="תת כותרת" style={{textAlign: "right"}}/>
             </div>
             
-            <div style={{ textAlign: "right" }}>
+            {/* <div style={{ textAlign: "right" }}>
                 <input className="input__content" type="test" name="password" placeholder="תוכן"
                     style={{textAlign: "right", width: "100rem", height: "20rem"}}/>
-            </div>
+            </div> */}
+
+            <Editor className="input__content"
+                initialValue="<p>תוכן הכתבה</p>"
+                init={{
+                height: 500,
+                menubar: false,
+                plugins: [
+                    'advlist autolink lists link image charmap print preview anchor',
+                    'searchreplace visualblocks code fullscreen',
+                    'insertdatetime media table paste code help wordcount'
+                ],
+                toolbar:
+                    'undo redo | formatselect | bold italic backcolor | \
+                    alignleft aligncenter alignright alignjustify | \
+                    bullist numlist outdent indent | removeformat | help'
+                }}
+                //onEditorChange={this.handleEditorChange}
+            />
             
             <div style={{ textAlign: "right" }}>
                 <input className="input__content" type="test" name="password" placeholder="מאת"
