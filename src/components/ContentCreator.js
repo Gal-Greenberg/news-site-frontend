7031421f5e7cfe410a8bbe6bc6dd6fa83082c117
GalZilca <galzilca@gmail.com>
2020-07-25 17:05:04 +0300

    create ContentCreator

--- src/components/ContentCreator.js
+++ src/components/ContentCreator.js
@@ -1,6 +1,32 @@
 import React from "react"
 import { Link } from 'react-router-dom';
 
+import { Header } from "./Header"
+
 export const ContentCreator = () => (
-    <p>ContentCreator</p>
+    <div>
+        <Header whithButtons={false}/>
+
+        <form className="form__content"/*onSubmit={}*/>
+            <div style={{ textAlign: "right" }}>
+                <input className="input__content" type="test" name="name" placeholder="כותרת" style={{textAlign: "right"}}/>
+            </div>
+
+            <div style={{ textAlign: "right" }}>
+                <input className="input__content" type="test" name="mail" placeholder="תת כותרת" style={{textAlign: "right"}}/>
+            </div>
+            
+            <div style={{ textAlign: "right" }}>
+                <input className="input__content" type="test" name="password" placeholder="תוכן"
+                    style={{textAlign: "right", width: "100rem", height: "20rem"}}/>
+            </div>
+            
+            <div style={{ textAlign: "right" }}>
+                <input className="input__content" type="test" name="password" placeholder="מאת"
+                    style={{textAlign: "right", width: "20rem"}}/>
+            </div>
+
+            <button className="button">פרסם</button>
+        </form>
+    </div>
 )
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,13 +1,13 @@
 import React from "react"
-import {Header} from "./Header"
+import { Header } from "./Header"
 
 export const SignUp = () => (
     <div>
         <Header whithButtons={false}/>
-        <form className="form"/*onSubmit={}*/>
-            <input className="input" type="test" name="name" placeholder="שם" style={{textAlign: "right"}}/>
-            <input className="input" type="test" name="mail" placeholder="מייל" style={{textAlign: "right"}}/>
-            <input className="input" type="test" name="password" placeholder="סיסמה" style={{textAlign: "right"}}/>
+        <form className="form__sign"/*onSubmit={}*/>
+            <input className="input__sign" type="test" name="name" placeholder="שם" style={{textAlign: "right"}}/>
+            <input className="input__sign" type="test" name="mail" placeholder="מייל" style={{textAlign: "right"}}/>
+            <input className="input__sign" type="test" name="password" placeholder="סיסמה" style={{textAlign: "right"}}/>
             <button className="button">הירשם</button>
         </form>
     </div>
